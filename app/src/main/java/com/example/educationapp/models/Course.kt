9d3a6a76a7fe1e04761931d9c818bc7d25031698
iiaman96 <jangraaman939@gmail.com>
2025-05-04 14:23:57 +0530

    Add app/src/main/java/com/example/educationapp/models/Course.kt 1746348061293

---
+++ app/src/main/java/com/example/educationapp/models/Course.kt
@@ -0,0 +1,11 @@
+package com.example.educationapp.models
+
+data class Course(
+    val id: String = "",
+    val title: String = "",
+    val description: String = "",
+    val thumbnailUrl: String = "",
+    val videoUrl: String = "",
+    val duration: String = "",
+    val instructor: String = ""
+)
